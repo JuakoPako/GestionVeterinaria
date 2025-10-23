f09ee7f0bff3799f2a000687c8a985e841e8e7cf
Joaquin Espinoza <bevodroid3@gmail.com>
2025-10-23 14:55:51 -0300

    Primer commit insanote

---
+++ src/model/Cita.java
@@ -0,0 +1,19 @@
+
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+public class Cita {
+    
+    private String nombreCliente;
+    private String nombreMascota;
+    private String veterinario;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private String motivo;
+    private boolean atendida;
+    
+    
+}
---
+++ src/model/Cliente.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Cliente {
+    
+    private String nombreUsuario;
+    private String contraseña;
+    private String nombreCompleto;
+    private String telefono;
+    private String correo;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombreUsuario, String contraseña, String nombreCompleto, String telefono, String correo) {
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+        this.nombreCompleto = nombreCompleto;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+    
+}
